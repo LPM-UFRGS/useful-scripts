e40a4d2d14801715552e5ac066bf734169f067bb
Áttila Leães Rodrigues <attila.leaes@gmail.com>
2016-06-28 15:07:47 -0300

    Print type of variables.

--- read_self_params_ar2gems_plugin_python/read_params.py
+++ read_self_params_ar2gems_plugin_python/read_params.py
@@ -2,9 +2,11 @@
 # para usa-la, use comando read_params(params) no iniitialize
 
 def read_params(a,j=''):
+  if j=='':
+    print "### Printing GUI parameters ###"
   for i in a:
     if (type(a[i])!=type({'a':1})):
-      print j+"['"+str(i)+"']="+str(a[i])
+      print j+"['"+str(i)+"']="+str(a[i]))+" type: "+str(type(a[i]))
     else:
       read_params(a[i],j+"['"+str(i)+"']")
 
