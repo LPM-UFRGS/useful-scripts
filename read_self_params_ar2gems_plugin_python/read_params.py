f4669bc7c2ca565ed9cf522cb9299db86d350c22
Áttila Leães Rodrigues <attila.leaes@gmail.com>
2017-06-01 13:08:28 -0300

    Update read_params.py
    minor bug

--- read_self_params_ar2gems_plugin_python/read_params.py
+++ read_self_params_ar2gems_plugin_python/read_params.py
@@ -6,7 +6,7 @@ def read_params(a,j=''):
     print "### Printing GUI parameters ###"
   for i in a:
     if (type(a[i])!=type({'a':1})):
-      print j+"['"+str(i)+"']="+str(a[i]))+" type: "+str(type(a[i]))
+      print j+"['"+str(i)+"']="+str(a[i])+" type: "+str(type(a[i]))
     else:
       read_params(a[i],j+"['"+str(i)+"']")
 
